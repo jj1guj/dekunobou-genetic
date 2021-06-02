cad402ec9c3052c236b2c733d9149db40827e516
jj1guj <57407826+jj1guj@users.noreply.github.com>
2021-06-02 22:11:09 +0900

    若干変更

--- engine.hpp
+++ engine.hpp
@@ -16,7 +16,9 @@ float eval_calc(Board board,int move,float param[param_size]){
     //相手の合法手をカウント
     LegalMoveList moves(board);
     for(int i=0;i<moves.size();++i)moves_opponent_sum+=param[moves[i]];
-    return out+param[64]*moves_opponent_sum+param[65]*(board.point[board.turn]-board.point[!board.turn]);
+    out+=param[64]*moves_opponent_sum;
+    out+=param[65]*(board.point[!board.turn]-board.point[board.turn]);//自分と相手の石の数の差
+    return out;
 }
 
 void minimax(Board board,float param[param_size],int depth){

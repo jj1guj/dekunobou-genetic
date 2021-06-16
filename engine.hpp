e0c0b165eddc54c9bb4fc53ca84745d8be6e4d1f
jj1guj <jj1guj@jj1guj.net>
2021-06-17 08:38:32 +0900

    αβ法の実装が多分できた

--- engine.hpp
+++ engine.hpp
@@ -57,6 +57,58 @@ float minimax(Board board,int depth){
     return eval;
 }
 
+//αβ法による先読み
+float alphabeta(Board board,int depth,float alpha,float beta){
+    //候補手の展開
+    LegalMoveList moves(board);
+    if(moves.size()==0){
+        //手番を変えて展開
+        board.push(-1);
+        LegalMoveList moves2(board);
+        //終局
+        if(moves2.size()==0){
+            return eval_calc(board);
+        }
+        moves=moves2;
+    }
+
+    float eval;
+    if(board.turn==turn_p)eval=-inf;//エンジン側が手番のときは評価値の最大値を求める
+    else eval=inf;//相手が手番のときは評価値の最小値を求める
+    //末端ノードのとき
+    Board board_ref;
+    if(depth<=0){
+        for(int i=0;i<moves.size();++i){
+            board_ref=board;
+            board_ref.push(moves[i]);
+            
+            if(board.turn==turn_p){
+                eval=std::max(eval,eval_calc(board_ref));
+            }else{
+                eval=std::min(eval,eval_calc(board_ref));
+            }
+        }
+        //std::cout<<eval<<std::endl;
+        return eval;
+    }
+
+    //それ以外のとき
+    for(int i=0;alpha<beta && i<moves.size();++i){
+        //1手打つ
+        board_ref=board;
+        board_ref.push(moves[i]);
+        eval=alphabeta(board_ref,depth-1,alpha,beta);
+
+        if(board.turn==turn_p && eval<beta){
+            beta=eval;//βカット
+        }else if(eval>alpha){
+            alpha=eval;///αカット
+        }
+    }
+    if(board.turn==turn_p)return beta;
+    else return alpha;
+}
+
 int go(Board board){
     float eval=-inf;
     LegalMoveList moves(board);
@@ -80,7 +132,9 @@ int go(Board board){
         //1手読み
         //eval_ref=eval_calc(board_ref);
         //先読みしてみる
-        eval_ref=minimax(board_ref,6);
+        //eval_ref=minimax(board_ref,6);
+        eval_ref=alphabeta(board_ref,8,-inf,inf);
+        std::cout<<i+1<<": "<<eval_ref<<std::endl;
         if(eval_ref>eval){
             bestmoves_num=0;
             BestMoves[bestmoves_num]=moves[i];

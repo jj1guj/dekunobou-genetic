5aef0b740534f6542a2fe9add5d07f1a6846a82d
jj1guj <jj1guj@jj1guj.net>
2021-06-22 22:07:07 +0900

    パラメータサイズを変更

--- dekunobou.hpp
+++ dekunobou.hpp
@@ -6,7 +6,10 @@
 #include<cstring>
 #include<string>
 #include<vector>
-#define param_size 66
+#define param_size 60
+#define cur_opening 0
+#define cur_middle 20
+#define cur_ending 40
 #define inf 1<<30
 #define ll long long
 

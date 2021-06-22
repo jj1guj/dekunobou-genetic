8c1eebd9a941173fcbd4efbab9e8d39c3f549bed
jj1guj <jj1guj@jj1guj.net>
2021-06-22 22:08:21 +0900

    探索中だと更新が面倒なので削除

--- dekunobou.hpp
+++ dekunobou.hpp
@@ -7,10 +7,5 @@
 #include<string>
 #include<vector>
 #define param_size 60
-#define cur_opening 0
-#define cur_middle 20
-#define cur_ending 40
 #define inf 1<<30
 #define ll long long
-
-extern int cur_offset;//序盤・中盤・終盤でパラメータの配列のどのインデックスから値を引っ張ってくればいいか計算する

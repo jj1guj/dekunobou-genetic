29ec6922d70a6a91bdac1da2fe1d3fd0cff89337
jj1guj <jj1guj@jj1guj.net>
2021-08-05 18:55:36 +0900

    新しい評価関数の仕様に変更

--- dekunobou.hpp
+++ dekunobou.hpp
@@ -2,13 +2,14 @@
 #include<chrono>
 #include<iostream>
 #include<limits>
+#include<map>
 #include<random>
 #include<cstring>
 #include<string>
 #include<vector>
-#define param_size 60
-#define cur_opening 0
-#define cur_middle 20
-#define cur_ending 40
+#define param_size 33
+#define cur_opening 27
+#define cur_middle 29
+#define cur_ending 30
 #define inf 1<<30
 #define ll long long
